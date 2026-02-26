187348a21797a116444e5b4f64613cb1234990fd
rafaelvictor01 <rafaelvictor.bernardes@gmail.com>
2026-02-26 18:00:00 -0300

    [#6]- [CONFIG] - Configurando o ESLint e o Prettier no projeto

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,23 +1,91 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-import'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
+    },
+
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+      import: importPlugin,
+      'jsx-a11y': jsxA11y
+    },
+
+    settings: {
+      react: { version: 'detect' },
+    },
+
+    rules: {
+      // A11y
+      ...jsxA11y.configs.recommended.rules,
+
+      // React
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+
+      // React Hooks
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // React Refresh
+      'react-refresh/only-export-components': 'warn',
+
+      // TS
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+
+      // Imports
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index']
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true
+          }
+        }
+      ],
+
+      // Prettier
+      'prettier/prettier': [
+        'error',
+        {
+          semi: false
+        }
+      ]
     },
   },
+
+  prettierConfig,
 ])
