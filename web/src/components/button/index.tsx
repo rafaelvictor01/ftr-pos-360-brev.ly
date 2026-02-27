ef01a4805e1af8b0ab8f7dd52839d379c55fd11c
rafaelvictor01 <rafaelvictor.bernardes@gmail.com>
2026-02-27 13:50:02 -0300

    [#13] - [FEAT] - Criando componente de botão

---
+++ web/src/components/button/index.tsx
@@ -0,0 +1,54 @@
+import { type IconProps } from "@phosphor-icons/react"
+import type { ComponentProps, ComponentType } from "react"
+import { tv, type VariantProps } from "tailwind-variants"
+
+const buttonVariants = tv({
+  base: "flex flex-row gap-3 items-center justify-center cursor-pointer transition-all duration-300 disabled:cursor-not-allowed",
+  variants: {
+    variant: {
+      primary:
+        "max-w-176 w-full bg-blue-base text-md text-white rounded-lg px-10 py-7.5 hover:not-disabled:bg-blue-dark disabled:opacity-50",
+      secondary:
+        "bg-gray-200 border border-gray-200 text-sm font-semibold text-gray-500 rounded-sm p-3.5 hover:not-disabled:border-blue-base disabled:opacity-50",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+  },
+})
+
+type ButtonProps = ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    icon?: ComponentType<IconProps>
+    isLoading?: boolean
+  }
+
+export function Button(props: ButtonProps) {
+  const variant = props.variant ?? "primary"
+  const Icon = props.icon
+  const isLoading = props.isLoading ?? false
+
+  const loadingColor =
+    variant === "primary" ? "border-gray-100" : "border-blue-base"
+
+  return (
+    <button type="button" className={buttonVariants({ variant })} {...props}>
+      {isLoading && (
+        <div
+          className={`w-6 h-6 border-2 ${loadingColor} border-t-white/40 rounded-full animate-spin`}
+          data-testid="button-loading"
+        />
+      )}
+
+      {Icon && !isLoading && (
+        <Icon
+          size="1rem"
+          color="var(--color-gray-600)"
+          data-testid="button-icon"
+        />
+      )}
+
+      {props.children}
+    </button>
+  )
+}
--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,7 +1,22 @@
+import logo from "../../assets/logo.svg"
+import { Button } from "../../components/button"
+
 export function HomePage() {
   return (
-    <>
-      <h1>Encurtador de URL</h1>
-    </>
+    <div className="w-full md:max-w-490 md:w-[calc(100vw - 1.5rem)] md:mt-25 flex flex-col items-center md:items-start gap-16">
+      <img
+        src={logo}
+        className="h-12"
+        alt="Logo do sistema com o nome 'brev.ly'"
+      />
+
+      <div className="w-full flex flex-col md:flex-row items-center md:items-start justify-center gap-6 md:gap-10">
+        <>
+          <Button variant="primary">Botão Primário</Button>
+        </>
+
+        <>a</>
+      </div>
+    </div>
   )
 }
